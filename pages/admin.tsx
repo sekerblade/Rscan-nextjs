411d2461817e6bc76f6db61686a457d0b0e51ab9
safe_pongsathorn <pongsathorn.sut@dome.tu.ac.th>
2023-06-26 20:01:08 +0700

    delete user page

--- pages/admin.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AdminPage: React.FC = () => {
-    return <h1>Welcome, Admin!</h1>;
-};
-
-export default AdminPage;
--- pages/user.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const UserPage: React.FC = () => {
-    return <h1>Welcome, User!</h1>;
-};
-
-export default UserPage;
