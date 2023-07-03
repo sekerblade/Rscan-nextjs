aff5b644f89eb3c6fa312b9533fedd2be7191cb8
Ponlaput petlorm <111440048+peechut@users.noreply.github.com>
2023-07-03 22:15:42 +0700

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,4 @@
-import mysql, { Connection, Pool, QueryError, RowDataPacket, OkPacket, ResultSetHeader, FieldPacket } from 'mysql2';
+import mysql, { Pool, QueryError, RowDataPacket, OkPacket, ResultSetHeader, FieldPacket, PoolConnection } from 'mysql2';
 
 // Create a connection pool
 const pool: Pool = mysql.createPool({
@@ -7,18 +7,26 @@ const pool: Pool = mysql.createPool({
     password: 'Pa$$w0rd',
     database: 'adgcoth_001',
     port: 3306,
-    connectionLimit: 10,
+    connectionLimit: 20, // Increase the connection limit if necessary
 });
 
 // Export a function to execute database queries
 export function query(sql: string, values?: any[]): Promise<any> {
     return new Promise((resolve, reject) => {
-        pool.query(sql, values, (error: QueryError | null, results: RowDataPacket[] | RowDataPacket[][] | OkPacket | OkPacket[] | ResultSetHeader, fields: FieldPacket[]) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(results);
+        pool.getConnection((err: NodeJS.ErrnoException | null, connection: PoolConnection) => {
+            if (err) {
+                reject(err);
+                return;
             }
+
+            connection.query(sql, values, (error: QueryError | null, results: RowDataPacket[] | RowDataPacket[][] | OkPacket | OkPacket[] | ResultSetHeader, fields: FieldPacket[]) => {
+                connection.release(); // Release the connection after executing the query
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(results);
+                }
+            });
         });
     });
 }
